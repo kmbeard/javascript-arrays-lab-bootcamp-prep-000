ec56eb9d4d3596309c47ccd89faa21a4fb83599b
Kelsey M. Beard <kay.beard.design@gmail.com>
2018-01-01 02:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push("Ralph");
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift("Bob");
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
